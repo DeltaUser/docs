5392a055d452a9668182735c970747ba7af97fb3
DeltaUser <DeltaUser@users.noreply.github.com>
2020-07-20 18:42:11 -0700

    space

--- html/index.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(async () => {
-    new Function(['particlesJS'], await (await fetch('https://raw.githubusercontent.com/DeltaUser/background/master/index.js')).text())(particlesJS);
-});
--- index.mjs
+++ index.mjs
@@ -113,7 +113,7 @@ async function getMDFILES(owner, name) {
 
 (async () => {
     app.listen(process.env.PORT || 5000, () => console.log(`[API] Listening to http://localhost:${process.env.PORT || 5000}/`));
-    app.get('/api/makeDocs', async (req, res) => {
+    app.get('/api/docs', async (req, res) => {
         const owner = req.query.owner;
         const name = req.query.name;
         if(!owner && !name) return res.send(300);
